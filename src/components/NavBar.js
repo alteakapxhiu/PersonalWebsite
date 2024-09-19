afeb36c237c14b136e5f80cf66d0920b9b78fe7f
Altea <alteakapxhiu@gmail.com>
2024-09-19 13:50:08 +0200

    Finishing Touches

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,6 +45,8 @@ export const NavBar = () => {
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
               <Nav.Link href="#Certifications" className={activeLink === 'Certifications' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Certifications')}>Certifications</Nav.Link>
+              <Nav.Link href="#connect" className={activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
+
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
