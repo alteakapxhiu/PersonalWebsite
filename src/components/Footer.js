402f5be3e9ebe9f7d6681907ae02cc6d6147c46a
Altea <alteakapxhiu@gmail.com>
2025-02-08 17:41:56 +0100

    Removed Data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,20 +24,20 @@ const Footer = () => {
   return (
     <MDBFooter className='text-center' color='white' bgColor='dark'>
       <MDBContainer className='p-4'>
-        
 
-        
 
-       <section className='mb-4'><section className='mb-4'>
-       <MDBBtn outline color="light" floating className='m-1' href='sms:+3550675548654?body=Hello%20there!' role='button'>
-  <MDBIcon icon="phone" />
-</MDBBtn>
+
+
+        <section className='mb-4'><section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://linktr.ee/alteakapxhiu' role='button'>
+            <MDBIcon icon="phone" />
+          </MDBBtn>
 
           <MDBBtn outline color="light" floating className='m-1' href='https://linktr.ee/alteakapxhiu' role='button'>
             <MDBIcon icon="globe" />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1'  href='mailto:alteakapxhiu@gmail.com' role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='mailto:alteakapxhiu@gmail.com' role='button'>
             <MDBIcon fab icon='google' />
           </MDBBtn>
 
@@ -53,60 +53,60 @@ const Footer = () => {
             <MDBIcon fab icon='github' />
           </MDBBtn>
         </section>
-            <section className=''>
-                
-          <form action=''>
-            <MDBRow className='d-flex justify-content-center'>
-              <MDBCol size="auto">
-                <p className='pt-2'>
-                  <strong>Don't forget to connect with me through my socials !</strong>
-                </p>
-              </MDBCol>
-              
-              <MDBCol size="auto">
-                <a href="https://www.linkedin.com/in/alteakapxhiu/" target="_blank" rel="noopener noreferrer">
+          <section className=''>
+
+            <form action=''>
+              <MDBRow className='d-flex justify-content-center'>
+                <MDBCol size="auto">
+                  <p className='pt-2'>
+                    <strong>Don't forget to connect with me through my socials !</strong>
+                  </p>
+                </MDBCol>
+
+                <MDBCol size="auto">
+                  <a href="https://www.linkedin.com/in/alteakapxhiu/" target="_blank" rel="noopener noreferrer">
                     <MDBBtn outline color="light" type="button" className="mb-4">
-                    Connect
+                      Connect
                     </MDBBtn>
-                </a>
-            </MDBCol>
+                  </a>
+                </MDBCol>
 
-              <MDBCol size="auto">
-                <a href="https://github.com/alteakapxhiu" target="_blank" rel="noopener noreferrer">
+                <MDBCol size="auto">
+                  <a href="https://github.com/alteakapxhiu" target="_blank" rel="noopener noreferrer">
                     <MDBBtn outline color="light" type="button" className="mb-4">
-                    Follow
+                      Follow
                     </MDBBtn>
-                </a>
-            </MDBCol>
+                  </a>
+                </MDBCol>
 
-              <MDBCol size="auto">
-              <MDBBtn outline color="light" type="button" className="mb-4" href="https://drive.google.com/uc?export=download&id=11oD8v2CuYcBA0hX1REPUVbmNnuAT7y06" target="_blank" role="button">
-  ⇩ Download CV
-</MDBBtn>
-            </MDBCol>
-      
-    
-              <MDBCol size="auto">
-              
-             
+                <MDBCol size="auto">
+                  <MDBBtn outline color="light" type="button" className="mb-4" href="https://drive.google.com/uc?export=download&id=11oD8v2CuYcBA0hX1REPUVbmNnuAT7y06" target="_blank" role="button">
+                    ⇩ Download CV
+                  </MDBBtn>
+                </MDBCol>
 
 
-    </MDBCol>
-            </MDBRow>
-          </form>
-        </section><hr/>
+                <MDBCol size="auto">
+
+
+
+
+                </MDBCol>
+              </MDBRow>
+            </form>
+          </section><hr />
           <p>
-          Professional web and mobile app developer specializing in delivering innovative and efficient digital solutions. For inquiries, collaborations, or to connect, please contact me.
+            Professional web and mobile app developer specializing in delivering innovative and efficient digital solutions. For inquiries, collaborations, or to connect, please contact me.
           </p>
         </section>
 
-        
+
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2024 Copyright: 
+        © 2024 Copyright:
         <a className='text-white' href='https://drive.google.com/file/d/11oD8v2CuYcBA0hX1REPUVbmNnuAT7y06/view?usp=drive_link'>
-          Altea Kapxhiu 
+          Altea Kapxhiu
         </a>
       </div>
     </MDBFooter>
